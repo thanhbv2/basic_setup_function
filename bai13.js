de5536efc94aaedf139ea6f205c947a4e9707e1f
thanh_buivan_vn <thanh.buivan@ascendcorp.com>
2019-08-02 21:34:06 +0700

    comment

--- bai13.js
+++ bai13.js
@@ -12,3 +12,10 @@ function uocSo(number) {
 console.log(uocSo(10))
 console.log(uocSo(15))
 console.log(uocSo(20))
+
+/**
+ * Kiểm tra điều kiện số phải dương
+ * Tìm ước của 1 số thì phải kiểm trả là số hay k ?
+ * bài này k bảo console.log ra
+ * lưu giá trị vào 1 mảng các ước
+ */
