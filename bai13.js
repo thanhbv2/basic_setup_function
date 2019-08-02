9ad26fb028e9dc355eff6ab2a821a4919b89bf8d
huy <huybui14022000@gmail.com>
2019-08-02 14:37:50 +0700

    tim uoc so

---
+++ bai13.js
@@ -0,0 +1,14 @@
+/// tim uoc so
+
+function uocSo(number) {
+
+    for (i = 0; i <= number; i++) {
+        if (number % i == 0) {
+            console.log(i)
+        }
+
+    }
+}
+console.log(uocSo(10))
+console.log(uocSo(15))
+console.log(uocSo(20))
